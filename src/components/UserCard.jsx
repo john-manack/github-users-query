6bba7ac23ad9eaa1a02d00118660580ad799f9b1
John Manack <jmnck28@gmail.com>
2021-03-02 12:18:06 -0500

    ensures user page opens in new window when link is selected

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,7 +4,7 @@ const UserCard = (props) => {
     const { user_info } = props;
     return (
         <>
-            <img src={user_info.avatar_url} alt={`${user_info.login}'s avatar`} className="avatar"></img>
+            <a href={user_info.html_url} rel="noreferrer" target="_blank"><img src={user_info.avatar_url} alt={`${user_info.login}'s avatar`} className="avatar"></img></a>
             <p>User Name: {user_info.login}</p>
             <p>Bio: {user_info.bio}</p>
         </>
